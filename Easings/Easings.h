27b07fe341d8fbe869673f2efc58d6ee15915b47
林達也 <t.hayashi0077+github@gmail.com>
2016-09-03 20:38:12 +0900

    Initial Commit

---
+++ Easings/Easings.h
@@ -0,0 +1,19 @@
+//
+//  Easings.h
+//  Easings
+//
+//  Created by 林達也 on 2016/09/03.
+//  Copyright © 2016年 jp.sora0077. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Easings.
+FOUNDATION_EXPORT double EasingsVersionNumber;
+
+//! Project version string for Easings.
+FOUNDATION_EXPORT const unsigned char EasingsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Easings/PublicHeader.h>
+
+
